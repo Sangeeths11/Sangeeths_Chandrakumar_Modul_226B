7cbacb8b1be1749967b4e723377779149828e4b0
Sangeeths Chandrakumar <sangeeths.chandrakumar@geocloud.ch>
2019-01-31 23:21:50 +0100

    InternationalesGate java Klasse fertig gestellt und bearbeitet

---
+++ src/AirportScheduler/InternationalesGate.java
@@ -0,0 +1,18 @@
+package AirportScheduler;
+
+public class InternationalesGate extends Gate{
+
+    public static final int KLEIN = 0;
+    public static final int GROSS = 1;
+
+    private int groesse;
+
+    public InternationalesGate(int nummer, int groesse){
+        super(nummer);
+        this.groesse = groesse;
+    }
+
+    public int getGroesse(){
+        return groesse;
+    }
+}
